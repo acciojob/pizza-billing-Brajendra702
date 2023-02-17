77fd3bc27674116101d57c20c841fcd744b095dc
Brajendra702 <vijendras452@gmail.com>
2023-02-17 14:18:18 +0530

    Pizza Solution

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -71,10 +71,10 @@ public class Pizza {
             if (isTakeAway) {
                 this.bill += "Paperbag Added: 20" + "\n";
             }
-            this.bill += "Total Price: " + this.price;
+            this.bill += "Total Price: " + this.price+"\n";
             this.isBillCreated = true;
             return this.bill;
         }
-        return "";
+        return this.bill;
     }
 }
